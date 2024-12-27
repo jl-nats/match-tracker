a0139621207ca5fa6ec5f5af6795b2e2b6eec05e
Stanley James <stanley.james@student.manchester.ac.uk>
2024-12-27 21:37:30 +0000

    complete working prototyp, TODO: refactor, error handling etc.

--- main.go
+++ main.go
@@ -1,9 +1,246 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+
+	"github.com/joho/godotenv"
 )
 
+var maps = map[string]string{"Sunset": "https://static.wikia.nocookie.net/valorant/images/5/5c/Loading_Screen_Sunset.png",
+	"Lotus":    "https://static.wikia.nocookie.net/valorant/images/d/d0/Loading_Screen_Lotus.png",
+	"Pearl":    "https://static.wikia.nocookie.net/valorant/images/a/af/Loading_Screen_Pearl.png",
+	"Fracture": "https://static.wikia.nocookie.net/valorant/images/f/fc/Loading_Screen_Fracture.png",
+	"Breeze":   "https://static.wikia.nocookie.net/valorant/images/1/10/Loading_Screen_Breeze.png",
+	"Icebox":   "https://static.wikia.nocookie.net/valorant/images/1/13/Loading_Screen_Icebox.png",
+	"Bind":     "https://static.wikia.nocookie.net/valorant/images/2/23/Loading_Screen_Bind.png",
+	"Haven":    "https://static.wikia.nocookie.net/valorant/images/7/70/Loading_Screen_Haven.png",
+	"Split":    "https://static.wikia.nocookie.net/valorant/images/d/d6/Loading_Screen_Split.png",
+	"Ascent":   "https://static.wikia.nocookie.net/valorant/images/e/e7/Loading_Screen_Ascent.png",
+	"Abyss":    "https://www.vpesports.com/wp-content/uploads/2024/06/Screenshot_4-19.png"}
+
+var WIN_COLOR = 0x2eb387
+var LOSS_COLOR = 0xff2832
+var DRAW_COLOR = 0x8BACB5
+
+func CreateEmbedFields(team []Player, label string, totalRounds int) []EmbedField {
+	teamFields := []EmbedField{}
+	teamFields = append(teamFields, EmbedField{
+		Name:   label + " Team",
+		Value:  "",
+		Inline: false})
+	for _, player := range team {
+		teamFields = append(teamFields, EmbedField{
+			Name:   player.Name + "#```" + player.Tag + "```",
+			Value:  "```" + strconv.Itoa(player.Stats.Score/(totalRounds)) + "|" + strconv.Itoa(player.Stats.Kills) + "-" + strconv.Itoa(player.Stats.Deaths) + "-" + strconv.Itoa(player.Stats.Assists) + "\n" + player.Agent.Name + "\n" + player.Tier.Name + "```",
+			Inline: true,
+		})
+	}
+	return teamFields
+}
+
+func SeparateTeams(players []Player) ([]Player, []Player) {
+	redTeam := []Player{}
+	blueTeam := []Player{}
+
+	for _, player := range players {
+		if player.Team == "Red" {
+			redTeam = append(redTeam, player)
+		} else {
+			blueTeam = append(blueTeam, player)
+		}
+	}
+
+	return redTeam, blueTeam
+}
+
+func LessFunc(team []Player) func(i, j int) bool {
+	return func(i, j int) bool {
+		return team[i].Stats.Score > team[j].Stats.Score
+	}
+}
+
+func CreateEmbed(matchData MatchData, trackedPlayerData TrackedPlayerData) Embed {
+	var trackedPlayer Player
+	for _, player := range matchData.Players {
+		if player.Name == trackedPlayerData.Name && player.Tag == trackedPlayerData.Tag {
+			trackedPlayer = player
+		}
+	}
+
+	embedColor, roundsWon, roundsLost, gameOutcome := newFunction(matchData, trackedPlayer)
+
+	redTeam, blueTeam := SeparateTeams(matchData.Players)
+
+	sort.Slice(redTeam, LessFunc(redTeam))
+
+	sort.Slice(blueTeam, LessFunc(blueTeam))
+
+	embedFields := append(CreateEmbedFields(redTeam, "red", roundsWon+roundsLost), CreateEmbedFields(blueTeam, "blue", roundsWon+roundsLost)...)
+
+	embed := Embed{
+		Title:       "🚨   NEW GAME " + trackedPlayerData.Name + "#" + trackedPlayerData.Tag + "   🚨",
+		Description: "**" + matchData.Metadata.Map.Name + "**" + " -- " + gameOutcome + " -- **" + strconv.Itoa(roundsWon) + " : " + strconv.Itoa(roundsLost) + "**\n\n",
+		Fields:      embedFields,
+		URL:         "https://tracker.gg/valorant/match/" + matchData.Metadata.MatchID,
+		Color:       embedColor,
+		Timestamp:   matchData.Metadata.StartedAt,
+		Image: EmbedImage{
+			URL:   maps[matchData.Metadata.Map.Name],
+			Width: 500,
+		},
+		Footer: EmbedFooter{
+			Text: matchData.Metadata.MatchID,
+		},
+	}
+	return embed
+}
+
+func newFunction(matchData MatchData, trackedPlayer Player) (int, int, int, string) {
+	embedColor := 0
+	gameState := 0
+	roundsWon := 0
+	roundsLost := 0
+
+	for _, team := range matchData.Teams {
+
+		playersTeam := false
+
+		if team.TeamID == trackedPlayer.Team {
+			playersTeam = true
+			roundsWon = team.Rounds.Won
+			roundsLost = team.Rounds.Lost
+		}
+
+		if team.Won {
+			if playersTeam {
+				gameState = 1
+			} else {
+				gameState = -1
+			}
+		}
+	}
+
+	var gameOutcome string
+	switch gameState {
+	case -1:
+		gameOutcome = "**DEFEAT**"
+		embedColor = LOSS_COLOR
+	case 1:
+		gameOutcome = "**VICTORY**"
+		embedColor = WIN_COLOR
+	case 0:
+		gameOutcome = "**DRAW**"
+		embedColor = DRAW_COLOR
+	}
+	return embedColor, roundsWon, roundsLost, gameOutcome
+}
+
+func executeWebhook(webhookURL string, matchData MatchData, trackedPlayerData TrackedPlayerData) {
+	var webhookData WebhookData = WebhookData{
+		Embeds: []Embed{CreateEmbed(matchData, trackedPlayerData)},
+	}
+	jsonData, err := json.Marshal(webhookData)
+	if err != nil {
+		log.Println("Error marshalling webhook data:", err)
+	}
+
+	res, err := http.Post(webhookURL, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		log.Println("Error making webhook request:", err)
+	}
+	defer res.Body.Close()
+}
+
+func getMatchData(client *http.Client, req *http.Request) MatchData {
+	res, err := client.Do(req)
+	if err != nil {
+		log.Println("Error making HD API request:", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		log.Println("Unexpected HD API response status:", res.Status)
+	}
+
+	matchDataResponse := handleRes(res)
+
+	return matchDataResponse.MatchData[0]
+}
+
+func handleRes(res *http.Response) MatchDataResponse {
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Println("Error reading HD API response body:", err)
+	}
+
+	var matchDataResponse MatchDataResponse
+
+	err = json.Unmarshal(body, &matchDataResponse)
+
+	if err != nil {
+		log.Println("Error unmarshalling HD API response body:", err)
+	}
+	return matchDataResponse
+}
+
+func createUrl(playerData TrackedPlayerData) string {
+	return "https://api.henrikdev.xyz/valorant/v4/matches/" + playerData.Region + "/" + playerData.Platform + "/" + playerData.Name + "/" + playerData.Tag + "?size=1?mode=competitive"
+}
+
 func main() {
-	fmt.Println("Hello, World!")
+	log.Println("START")
+	log.Println("Loading environmental variables...")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	log.Println("Loading environmental variables...")
+	WEBHOOK_URL := os.Getenv("WEBHOOK_URL")
+	API_KEY := os.Getenv("API_KEY")
+
+	var playerData TrackedPlayerData = TrackedPlayerData{
+		Name:     os.Getenv("PLAYER_NAME"),
+		Tag:      os.Getenv("PLAYER_TAG"),
+		Platform: os.Getenv("PLAYER_PLATFORM"),
+		Region:   os.Getenv("PLAYER_REGION"),
+	}
+
+	client := &http.Client{}
+
+	apiUrl := createUrl(playerData)
+
+	req, err := http.NewRequest("GET", apiUrl, nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req.Header.Add("Authorization", API_KEY)
+
+	log.Println("Server Started")
+	var lastMatchID string
+
+	for {
+		log.Println("Checking match data...")
+		matchData := getMatchData(client, req)
+
+		if matchData.Metadata.MatchID == lastMatchID {
+			log.Println("No new match data found")
+		} else {
+			lastMatchID = matchData.Metadata.MatchID
+			log.Println("New match found. Executing webhook...")
+			executeWebhook(WEBHOOK_URL, matchData, playerData)
+		}
+
+		time.Sleep(10 * time.Second)
+
+	}
 }
---
+++ models.go
@@ -0,0 +1,83 @@
+package main
+
+type Team struct {
+	TeamID string `json:"team_id"`
+	Rounds struct {
+		Won  int `json:"won"`
+		Lost int `json:"lost"`
+	}
+	Won bool `json:"won"`
+}
+
+type Player struct {
+	Name  string `json:"name"`
+	Tag   string `json:"tag"`
+	Team  string `json:"team_id"`
+	Agent struct {
+		Name string `json:"name"`
+	} `json:"agent"`
+	Stats struct {
+		Score   int `json:"score"`
+		Kills   int `json:"kills"`
+		Deaths  int `json:"deaths"`
+		Assists int `json:"assists"`
+	} `json:"stats"`
+	Tier struct {
+		Name string `json:"name"`
+	} `json:"tier"`
+}
+
+type MatchDataResponse struct {
+	Status    int         `json:"status"`
+	MatchData []MatchData `json:"data"`
+}
+
+type WebhookData struct {
+	Embeds []Embed `json:"embeds"`
+}
+
+type Embed struct {
+	Title       string       `json:"title"`
+	Description string       `json:"description"`
+	URL         string       `json:"url"`
+	Color       int          `json:"color"`
+	Timestamp   string       `json:"timestamp"`
+	Image       EmbedImage   `json:"image"`
+	Fields      []EmbedField `json:"fields"`
+	Footer      EmbedFooter  `json:"footer"`
+}
+
+type EmbedImage struct {
+	URL   string `json:"url"`
+	Width int    `json:"height"`
+}
+
+type EmbedFooter struct {
+	Text    string `json:"text"`
+	IconURL string `json:"icon_url"`
+}
+
+type EmbedField struct {
+	Name   string `json:"name"`
+	Value  string `json:"value"`
+	Inline bool   `json:"inline"`
+}
+
+type MatchData struct {
+	Metadata struct {
+		MatchID string `json:"match_id"`
+		Map     struct {
+			Name string `json:"name"`
+		} `json:"map"`
+		StartedAt string `json:"started_at"`
+	} `json:"metadata"`
+	Players []Player
+	Teams   []Team `json:"teams"`
+}
+
+type TrackedPlayerData struct {
+	Name     string
+	Tag      string
+	Platform string
+	Region   string
+}
